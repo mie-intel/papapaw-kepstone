ef6557c6cf7241bd6abb2469f1b4342b2f13d1ac
Timothy Tanuwidjaya <timothytanuwidjaya@mail.ugm.ac.id>
2025-09-14 19:04:49 +0700

    Optimisasi Struktur Folder

---
+++ backend/models/objectModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const {type} = require('os');
+
+const pekerjaSchema = new mongoose.Schema({
+        nomorInduk: { type: String, required: true, unique: true },
+        nama: { type: String, required: true },
+        jabatan: { type: String, required: true },
+        departemen: { type: String, required: true },
+        username: { type: String, required: true, unique: true },
+        password
+    })
+
+const laporanSchema = new mongoose.Schema({
+        idSurat: { type: String, required: true, unique: true },
+        tanggal: { type: Date, required: true },
+        skalaCedera: {type: Int16Array, required: true },
+        detail: { type: String, required: true },
+        status: { type: Int16Array, required: true },
+        lokasi: { type: String, required: true },
+        pesanKesalahan: { type: String, required: false },
+        tertolak: {type: Boolean, required: true },
+    });
+
+const Pekerja = mongoose.model('Pekerja', pekerjaSchema);
+const Laporan = mongoose.model('Laporan', laporanSchema);
+
+module.exports = { Pekerja, Laporan };
