447753c3a13eaaf794e34adbdf1cf56f2e067d98
mie-intel <paryapradhanika@gmail.com>
2025-11-13 16:36:45 +0700

    fix(middleware): disable middleware

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,41 +1,41 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const token = request.cookies.get("token")?.value;
-  console.log("Middleware - Token:", token);
-  console.log("Middleware - Pathname:", request.nextUrl.pathname);
-  if (!token && request.nextUrl.pathname !== "/login" && request.nextUrl.pathname !== "/register") {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-  if (token) {
-    const role = request.cookies.get("role")?.value;
-    const { pathname } = request.nextUrl;
-    if (
-      role === "HSE" &&
-      (pathname.startsWith("/kepala") ||
-        pathname.startsWith("/direktur") ||
-        pathname === "/login" ||
-        pathname === "/register")
-    ) {
-      return NextResponse.redirect(new URL("/hse/dashboard", request.url));
-    }
-    if (
-      role === "Kepala Bagian" &&
-      (pathname.startsWith("/hse") ||
-        pathname.startsWith("/direktur" || pathname === "/login" || pathname === "/register"))
-    ) {
-      return NextResponse.redirect(new URL("/kepala/dashboard", request.url));
-    }
-    if (
-      role === "Direktur" &&
-      (pathname.startsWith("/hse") ||
-        pathname.startsWith("/kepala") ||
-        pathname === "/login" ||
-        pathname === "/register")
-    ) {
-      return NextResponse.redirect(new URL("/direktur/dashboard", request.url));
-    }
-  }
+  // const token = request.cookies.get("token")?.value;
+  // console.log("Middleware - Token:", token);
+  // console.log("Middleware - Pathname:", request.nextUrl.pathname);
+  // if (!token && request.nextUrl.pathname !== "/login" && request.nextUrl.pathname !== "/register") {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
+  // if (token) {
+  //   const role = request.cookies.get("role")?.value;
+  //   const { pathname } = request.nextUrl;
+  //   if (
+  //     role === "HSE" &&
+  //     (pathname.startsWith("/kepala") ||
+  //       pathname.startsWith("/direktur") ||
+  //       pathname === "/login" ||
+  //       pathname === "/register")
+  //   ) {
+  //     return NextResponse.redirect(new URL("/hse/dashboard", request.url));
+  //   }
+  //   if (
+  //     role === "Kepala Bagian" &&
+  //     (pathname.startsWith("/hse") ||
+  //       pathname.startsWith("/direktur" || pathname === "/login" || pathname === "/register"))
+  //   ) {
+  //     return NextResponse.redirect(new URL("/kepala/dashboard", request.url));
+  //   }
+  //   if (
+  //     role === "Direktur" &&
+  //     (pathname.startsWith("/hse") ||
+  //       pathname.startsWith("/kepala") ||
+  //       pathname === "/login" ||
+  //       pathname === "/register")
+  //   ) {
+  //     return NextResponse.redirect(new URL("/direktur/dashboard", request.url));
+  //   }
+  // }
 
   return NextResponse.next();
 }
